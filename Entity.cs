65d11c07ce3ada79ef48a3473a5955162990d83c
Zeb Hicks <Nimphious@gmail.com>
2017-10-18 09:13:53 +1100

    Add gitignore and Entity class.

---
+++ Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceGame {
+    
+    class Entity {
+
+    }
+}
